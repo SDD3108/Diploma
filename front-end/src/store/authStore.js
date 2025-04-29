30700662613ae29a6903327aa1c74d3704a8d47e
SD3108 <mrbimson1@gmail.com>
2025-04-29 18:24:17 +0500

    nineteen

--- front-end/src/store/authStore.js
+++ front-end/src/store/authStore.js
@@ -1,6 +1,6 @@
 import create from 'zustand';
 
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set)=>({
     user: null,
     isLoading: false,
     error: null,
