bdc5cbdee8423f8bef0505bb6ea3dd66dede3647
SDD3108 <mrbimson1@gmail.com>
2025-05-15 19:15:34 +0500

    add + update

---
+++ front-end/src/utils/CheckDisplaySize/CheckDisplaySize.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+export function CheckDisplaySize(breakpoint = 240){
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(()=>{
+    const onResize = () => setIsMobile(window.innerWidth < breakpoint)
+    onResize()
+    window.addEventListener('resize', onResize)
+    return () => window.removeEventListener('resize', onResize)
+  },[breakpoint])
+
+  return isMobile
+}
